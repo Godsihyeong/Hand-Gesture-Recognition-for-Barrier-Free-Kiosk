e71ee59b00efad487cc30db9a194041e14f2c42b
Godsihyeong <105179996+Godsihyeong@users.noreply.github.com>
2024-09-25 00:59:07 +0900

    Add files via upload

--- install_packages.py
+++ install_packages.py
@@ -8,3 +8,4 @@ def install_package(package):
     except ImportError:
         print(f"{package} 라이브러리가 설치되어 있지 않습니다. 설치를 시작합니다...")
         subprocess.check_call([sys.executable, "-m", "pip", "install", package])
+        print('설치완료')
